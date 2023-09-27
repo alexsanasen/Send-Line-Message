e7734abf858f40b017e264d78c5ce5f272ccd9c0
Alex Sanasen <alex.sanasen@gmail.com>
2023-09-26 22:27:10 -0700

    nc

--- sendline.py
+++ sendline.py
@@ -1,23 +1,24 @@
 #Program: Send Line Message
 #Date: 2023.09.25
 
-#Message
+#Message Function
 def lineNotify(message):
     payload = {'message':message}
     return _lineNotify(payload)
 
-#Sticker
+#Sticker Function
 #URL: https://developers.line.biz/en/docs/messaging-api/sticker-list/
 def notifySticker(stickerID,stickerPackageID):
     payload = {'message':" ",'stickerPackageId':stickerPackageID,'stickerId':stickerID}
     return _lineNotify(payload)
 
-#Image
+#Image Function
+#URL: https://uppic.cloud
 def notifyPicture(url):
     payload = {'message':" ",'imageThumbnail':url,'imageFullsize':url}
     return _lineNotify(payload)
 
-#Notification
+#Notification Function
 def _lineNotify(payload,file=None):
     import requests
     url = 'https://notify-api.line.me/api/notify'
@@ -25,8 +26,8 @@ def _lineNotify(payload,file=None):
     headers = {'Authorization':'Bearer '+token}
     return requests.post(url, headers=headers , data = payload, files=file)
 
-mymessage = "Hello World"
-
+#Sending Message
+mymessage = "สวัสดีวันอังคาร"
 lineNotify(mymessage)
 notifySticker(11088016,6370)
-#notifyPicture("ที่อยู่รูปภาพ")
+#notifyPicture("Image URL")
