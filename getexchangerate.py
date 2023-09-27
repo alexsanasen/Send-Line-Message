b6ea0c6287702d4c839f9dffcd9337c10bd5deaf
Alex Sanasen <alex.sanasen@gmail.com>
2023-09-26 22:43:24 -0700

    nc

--- getexchangerate.py
+++ getexchangerate.py
@@ -3,12 +3,12 @@
 
 import requests
 
-# Where USD is the base currency you want to use
-url = 'https://v6.exchangerate-api.com/v6/287aeae054b3dbf60085bd87/latest/CAD'
+# Where CAD is the base currency and THB is the target currency you want to use
+url = 'https://v6.exchangerate-api.com/v6/287aeae054b3dbf60085bd87/pair/CAD/THB'
 
 # Making our request
 response = requests.get(url)
 data = response.json()
 
 # Your JSON object
-print data
+print (data)
